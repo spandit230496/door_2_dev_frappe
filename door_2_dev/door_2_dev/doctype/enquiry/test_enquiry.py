18580f440214ba422f64a05633408c762a2681f4
sandip.pandit <sandip.pandit@redsoftware.in>
2025-12-19 08:50:31 +0000

    add:initial doctypes

---
+++ door_2_dev/door_2_dev/doctype/enquiry/test_enquiry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sandip pandit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEnquiry(FrappeTestCase):
+	pass
